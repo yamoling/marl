0f91e84084311038351131441fd50a55ed41a51f
Yannick Molinghen <yannick.molinghen@ulb.be>
2023-12-14 17:50:29 +0100

    vdn

--- src/marl/qlearning/mixers/vdn.py
+++ src/marl/qlearning/mixers/vdn.py
@@ -3,14 +3,12 @@ import torch
 from .mixer import Mixer
 
 
-
 class VDN(Mixer):
     def forward(self, qvalues: torch.Tensor, *_) -> torch.Tensor:
-        return qvalues.sum(dim=-1)
-    
+        return qvalues.sum(dim=-1, keepdim=True)
+
     def save(self, directory: str):
         return
-    
+
     def load(self, directory: str):
         return
-    
