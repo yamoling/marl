fb9d39658677686b8dc57e41b991ed0d3a558f23
Yannick Molinghen <yannick.molinghen@ulb.be>
2024-02-05 12:06:39 +0100

    ready to test on server

--- src/create_experiments.py
+++ src/create_experiments.py
@@ -12,52 +12,56 @@ def create_experiments():
     level = "lvl6"
     level = f"maps/normal/{level}"
     n_steps = 1_000_000
-    for n in range(3, 11, 2):
-        env = StaticLaserEnv(level, ObservationType.LAYERED)
-        time_limit = round(env.width * env.height / 2)
-        env, test_env = rlenv.Builder(env).agent_id().time_limit(time_limit).build_all()
-        
-        # E-greedy decreasing from 1 to 0.05 over 100_000 update steps
-        train_policy = marl.policy.EpsilonGreedy.linear(1, 0.05, 100_000)
-        # train_policy = marl.policy.EpsilonGreedy.constant(0.05)
-        test_policy = marl.policy.ArgMax()
+    env = StaticLaserEnv(level, ObservationType.LAYERED)
+    time_limit = round(env.width * env.height / 2)
+    env, test_env = rlenv.Builder(env).agent_id().time_limit(time_limit).build_all()
 
-        qnetwork = marl.nn.model_bank.CNN.from_env(env)
-        mixer = marl.qlearning.mixers.VDN(env.n_agents)
-        # mixer = marl.qlearning.mixers.QMix(env.state_shape[0], env.n_agents)
-        # memory = marl.models.TransitionMemory(memory_size)
-        memory = marl.models.NStepMemory(memory_size, n, gamma)
+    # E-greedy decreasing from 1 to 0.05 over 100_000 update steps
+    train_policy = marl.policy.EpsilonGreedy.linear(1, 0.05, 100_000)
+    # train_policy = marl.policy.EpsilonGreedy.constant(0.05)
+    test_policy = marl.policy.ArgMax()
 
-        ir = marl.intrinsic_reward.RandomNetworkDistillation(
-            env.observation_shape, 
-            env.extra_feature_shape, 
-            update_ratio=0.25,
-            clip_value=1,
-            ir_weight=Schedule.linear(2.0, 0, 300_000)
-        )
+    qnetwork = marl.nn.model_bank.CNN.from_env(env)
+    mixer = marl.qlearning.mixers.VDN(env.n_agents)
+    # mixer = marl.qlearning.mixers.QMix(env.state_shape[0], env.n_agents)
+    memory = marl.models.TransitionMemory(memory_size)
+    # memory = marl.models.NStepMemory(memory_size, n, gamma)
 
-        algo = marl.qlearning.LinearMixedDQN(
-            qnetwork=qnetwork,
-            batch_size=batch_size,
-            train_policy=train_policy,
-            test_policy=test_policy,
-            gamma=gamma,
-            memory=memory,
-            mixer=mixer,
-            train_interval=5,
-            ir_module=ir
-        )
+    ir = marl.intrinsic_reward.RandomNetworkDistillation(
+        env.observation_shape,
+        env.extra_feature_shape,
+        update_ratio=0.25,
+        clip_value=1,
+        ir_weight=Schedule.linear(2.0, 0, 300_000),
+    )
 
-        name = level
-        if level.startswith("maps/"):
-            name = level[-4:]
-        logdir = f"logs/{name}-{mixer.name}-{memory.name}-{memory._n}"
-        #logdir = "test"
-        exp = Experiment.create(logdir, algo=algo, env=env, test_interval=5000, test_env=test_env, n_steps=n_steps)
-        # exp.create_runner().train(1)
-        print("Created experiment:", exp.logdir)
+    algo = marl.qlearning.LinearMixedDQN(
+        qnetwork=qnetwork,
+        batch_size=batch_size,
+        train_policy=train_policy,
+        test_policy=test_policy,
+        gamma=gamma,
+        memory=memory,
+        mixer=mixer,
+        train_interval=5,
+        # ir_module=ir,
+    )
 
-
+    name = level
+    if level.startswith("maps/"):
+        name = level[-4:]
+    logdir = f"logs/bnaic-{name}-{mixer.name}"
+    # logdir = "test"
+    exp = Experiment.create(
+        logdir,
+        algo=algo,
+        env=env,
+        test_interval=5000,
+        test_env=test_env,
+        n_steps=n_steps,
+    )
+    # exp.create_runner().train(1)
+    print("Created experiment:", exp.logdir)
 
 
 if __name__ == "__main__":
