929d1323b7646fa81d73e4ba92f9aef626c47a84
Yannick Molinghen <yannick.molinghen@ulb.be>
2024-03-19 16:12:37 +0100

    Restored batches to previous working

--- src/marl/models/batch/episode_batch_old.py
+++ src/marl/models/batch/episode_batch_old.py
@@ -5,7 +5,7 @@ from rlenv import Episode
 from .batch import Batch
 
 
-class EpisodeBatch(Batch):
+class EpisodeBatchOld(Batch):
     def __init__(self, episodes: list[Episode]):
         super().__init__(len(episodes), episodes[0].n_agents)
         self._max_episode_len = max(len(e) for e in episodes)
@@ -34,28 +34,20 @@ class EpisodeBatch(Batch):
     #     return normalized_returns
 
     @cached_property
-    def obs_(self):
-        """
-        All the observations of the episodes, from 0 to episode_length + 1.
-
-        The reason why we have episode_length + 1 is because we need the first observation of the episode
-        to perform a correct inference right from the start.
-        """
+    def all_obs(self):
         return torch.from_numpy(np.array([e._observations for e in self.episodes], dtype=np.float32)).transpose(1, 0).to(self.device)
 
     @cached_property
-    def extras_(self):
-        """All the extras of the episodes, from 0 to episode_length + 1"""
+    def all_extras(self):
         return torch.from_numpy(np.array([e._extras for e in self.episodes], dtype=np.float32)).transpose(1, 0).to(self.device)
 
     @cached_property
-    def obs(self):
-        obs = np.array([e.obs.data for e in self.episodes], dtype=np.float32)
-        return torch.from_numpy(obs).transpose(1, 0).to(self.device)
+    def all_available_actions(self):
+        return torch.from_numpy(np.array([e._available_actions for e in self.episodes], dtype=np.int64)).transpose(1, 0).to(self.device)
 
     @cached_property
-    def extras(self):
-        return torch.from_numpy(np.array([e.extras for e in self.episodes], dtype=np.float32)).transpose(1, 0).to(self.device)
+    def all_states(self):
+        return torch.from_numpy(np.array([e._states for e in self.episodes], dtype=np.float32)).transpose(1, 0).to(self.device)
 
     @cached_property
     def actions(self):
@@ -69,22 +61,6 @@ class EpisodeBatch(Batch):
     @cached_property
     def dones(self):
         return torch.from_numpy(np.array([e.dones for e in self.episodes], dtype=np.float32)).transpose(1, 0).to(self.device)
-
-    @cached_property
-    def available_actions_(self):
-        return torch.from_numpy(np.array([e.available_actions_ for e in self.episodes], dtype=np.int64)).transpose(1, 0).to(self.device)
-
-    @cached_property
-    def available_actions(self):
-        return torch.from_numpy(np.array([e.available_actions for e in self.episodes], dtype=np.int64)).transpose(1, 0).to(self.device)
-
-    @cached_property
-    def states(self):
-        return torch.from_numpy(np.array([e.states[:-1] for e in self.episodes], dtype=np.float32)).transpose(1, 0).to(self.device)
-
-    @cached_property
-    def states_(self):
-        return torch.from_numpy(np.array([e.states[1:] for e in self.episodes], dtype=np.float32)).transpose(1, 0).to(self.device)
 
     @cached_property
     def masks(self):
--- src/marl/models/batch/transition_batch_old.py
+++ src/marl/models/batch/transition_batch_old.py
@@ -6,26 +6,30 @@ from rlenv import Transition
 from .batch import Batch
 
 
-class TransitionBatch(Batch):
+class TransitionBatchOld(Batch):
     def __init__(self, transitions: list[Transition]):
         super().__init__(len(transitions), transitions[0].n_agents)
         self.transitions = transitions
 
     @cached_property
-    def obs(self):
-        return torch.from_numpy(np.array([t.obs.data for t in self.transitions], dtype=np.float32)).to(self.device)
+    def all_obs(self):
+        observations = [t.obs.data for t in self.transitions] + [self.transitions[-1].obs_.data]
+        return torch.from_numpy(np.array(observations, dtype=np.float32)).to(self.device)
 
     @cached_property
-    def obs_(self):
-        return torch.from_numpy(np.array([t.obs_.data for t in self.transitions], dtype=np.float32)).to(self.device)
+    def all_extras(self):
+        extras = [t.obs.extras for t in self.transitions] + [self.transitions[-1].obs_.extras]
+        return torch.from_numpy(np.array(extras, dtype=np.float32)).to(self.device)
 
     @cached_property
-    def extras(self):
-        return torch.from_numpy(np.array([t.obs.extras for t in self.transitions], dtype=np.float32)).to(self.device)
+    def all_available_actions(self):
+        available_actions = [t.obs.available_actions for t in self.transitions] + [self.transitions[-1].obs_.available_actions]
+        return torch.from_numpy(np.array(available_actions, dtype=np.int64)).to(self.device)
 
     @cached_property
-    def extras_(self):
-        return torch.from_numpy(np.array([t.obs_.extras for t in self.transitions], dtype=np.float32)).to(self.device)
+    def all_states(self):
+        states = [t.obs.state for t in self.transitions] + [self.transitions[-1].obs_.state]
+        return torch.from_numpy(np.array(states, dtype=np.float32)).to(self.device)
 
     @cached_property
     def actions(self):
@@ -40,14 +44,6 @@ class TransitionBatch(Batch):
     @cached_property
     def dones(self):
         return torch.from_numpy(np.array([t.done for t in self.transitions], dtype=np.float32)).to(self.device)
-
-    @cached_property
-    def available_actions(self):
-        return torch.from_numpy(np.array([t.obs.available_actions for t in self.transitions], dtype=np.int64)).to(self.device)
-
-    @cached_property
-    def available_actions_(self):
-        return torch.from_numpy(np.array([t.obs_.available_actions for t in self.transitions], dtype=np.int64)).to(self.device)
 
     @cached_property
     def states(self):
