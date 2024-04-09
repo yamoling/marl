2b9c64e7b02a55ae4935dca243c2aa200263377b
Yannick Molinghen <yannick.molinghen@ulb.be>
2024-04-09 22:52:58 +0200

    wip

--- src/marl/env/ued_lle.py
+++
@@ -1,8 +0,0 @@
-from rlenv import DiscreteSpace, RLEnv
-from lle import World
-
-
-class UED_LLE(RLEnv):
-    def __init__(self, width: int, height: int, num_agents: int):
-        action_space = DiscreteSpace()
-        super().__init__(action_space, observation_shape, state_shape, extra_feature_shape, reward_space)
