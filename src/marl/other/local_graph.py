2cd39461d947b861eea06c529ae521cbd17d02b5
Yannick Molinghen <yannick.molinghen@ulb.be>
2024-11-08 16:06:18 +0100

    Local graph 2 agents OK

--- src/marl/other/local_graph.py
+++ src/marl/other/local_graph.py
@@ -1,56 +1,58 @@
 import networkx as nx
 import numpy as np
 from sklearn.cluster import SpectralClustering
-from marlenv import Transition, Episode
+from marlenv import Transition
 from torch import device
 from marl.training import Trainer
+from lle import World, WorldState
 
 
 WEIGHT = "weight"
 
 
-class LocalGraphBottleneckFinder:
+class LocalGraphBottleneckFinder[T]:
     """
     Find bottlenecks via local graph building and spectral clustering.
     https://dl.acm.org/doi/10.1145/1102351.1102454
     """
 
     def __init__(self, n_apparition_threshold: int = 10, hit_ratio_threshold: float = 0.5):
-        self.edge_occurrences = dict[tuple[int, int], int]()
+        self.edge_occurrences = dict[tuple[T, T], int]()
         """Map each edge to its weight (int)"""
         self.t_o = n_apparition_threshold
         """Threshold for the minimum number of apparitions of a state before considering it as a potential bottleneck"""
         self.t_h = hit_ratio_threshold
         """Threshold for the minimum hit ratio of a state before considering it as a bottleneck"""
-        self.hit_count = dict[tuple[int, int], int]()
+        self.hit_count = dict[tuple[T, T], int]()
         """Number of times an edge was classified as a bottleneck"""
-        self.apparition_count = dict[tuple[int, int], int]()
+        self.apparition_count = dict[tuple[T, T], int]()
         """Number of times an edge was encountered in the construction of the local graph"""
 
-    def is_bottleneck(self, edge: tuple[int, int]):
+    def is_bottleneck(self, edge: tuple[T, T]):
         ratio = self.predict(edge)
         return ratio >= self.t_h
 
-    def predict(self, edge: tuple[int, int]):
+    def predict(self, edge: tuple[T, T]):
         n = self.apparition_count.get(edge, 0)
-        if n < self.t_o:
-            return 0.0
+        # if n < self.t_o:
+        #     return 0.0
         return self.hit_count.get(edge, 0) / n
 
-    def _build_local_graph(self, episode: Episode):
+    def _build_local_graph(self, states: list[T]):
         local_graph = nx.Graph()
-        for transition in episode.transitions():
-            prev_bytes = transition.obs.state.data.tobytes()
-            prev_state = hash(prev_bytes)
-            next_bytes = transition.obs_.state.data.tobytes()
-            next_state = hash(next_bytes)
+        for i in range(len(states) - 1):
+            prev_state = states[i]
+            next_state = states[i + 1]
             edge = prev_state, next_state
-            self.edge_occurrences[edge] = self.edge_occurrences.get(edge, 0) + 1
-        local_graph.add_weighted_edges_from((src, dest, v) for (src, dest), v in self.edge_occurrences.items())
+            weight = self.edge_occurrences.get(edge, 0) + 1
+            self.edge_occurrences[edge] = weight
+            local_graph.add_edge(prev_state, next_state, weight=weight)
+        # local_graph.add_weighted_edges_from((src, dest, v) for (src, dest), v in self.edge_occurrences.items())
         return local_graph
 
-    def compute_bottleneck(self, episode: Episode):
-        local_graph = self._build_local_graph(episode)
+    def compute_bottleneck(self, states: list[T]):
+        local_graph = self._build_local_graph(states)
+        # print(f"Graph size: {len(local_graph.nodes)} nodes, {len(local_graph.edges)} edges")
         W = nx.adjacency_matrix(local_graph).todense()  # Adjacency/Weight matrix of the graph
         sc = SpectralClustering(2, affinity="precomputed", n_init=100)
         labels = sc.fit_predict(W)
@@ -92,20 +94,25 @@ class LocalGraphBottleneckFinder:
 
 
 class LocalGraphTrainer(Trainer):
-    def __init__(self, wrapped_trainer: Trainer | None, local_graph: LocalGraphBottleneckFinder):
+    def __init__(self, local_graph: LocalGraphBottleneckFinder[WorldState], world: World, wrapped_trainer: Trainer | None = None):
         super().__init__("both")
         self.trainer = wrapped_trainer
         self.local_graph = local_graph
+        self.world = world
+        self.states = list[WorldState]()
 
     def update_step(self, transition: Transition, time_step: int):
+        self.states.append(self.world.get_state())
         if self.trainer is not None:
             return self.trainer.update_step(transition, time_step)
         return {}
 
     def update_episode(self, episode, episode_num, time_step):
-        self.local_graph.compute_bottleneck(episode)
+        self.local_graph.compute_bottleneck(self.states)
+        self.states.clear()
         if self.trainer is not None:
-            return self.trainer.update_episode(episode, episode_num, time_step)
+            logs = self.trainer.update_episode(episode, episode_num, time_step)
+            return logs
         return {}
 
     def to(self, device: device):
