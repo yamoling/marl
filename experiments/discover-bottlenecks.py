29853ca4fbdbfb392b3c3932f128bb33da405520
Yannick Molinghen <yannick.molinghen@ulb.be>
2024-11-12 17:24:58 +0100

    wip

--- experiments/discover-bottlenecks.py
+++ experiments/discover-bottlenecks.py
@@ -1,9 +1,7 @@
 from lle import LLE, WorldState, World
 import marl
-import os
-import shutil
 import json
-from torch.multiprocessing import Pool
+from multiprocessing.pool import Pool, AsyncResult
 import marlenv
 import numpy as np
 from collections import defaultdict
@@ -33,38 +31,36 @@ def compute_heatmap(world: World, finder: LocalGraphBottleneckFinder[WorldState]
     return heatmap
 
 
-def run_experiment(exp: marl.Experiment, seed: int):
-    print(f"Running {exp.logdir}")
+def run_experiment(logdir: str, seed: int):
+    exp = marl.Experiment.load(logdir)
     trainer: LocalGraphTrainer = exp.trainer  # type: ignore
     finder = trainer.local_graph
     world = trainer.world
 
     exp.run(seed=seed, device="auto")
     heatmap = compute_heatmap(world, finder)
-    filename = f"{exp.logdir}/heatmap-{seed}.json"
+    filename = f"{logdir}/heatmap-{seed}.json"
     with open(filename, "w") as f:
         json.dump(heatmap.tolist(), f)
 
 
-def run_experiments(experiments: list[marl.Experiment], n_seeds: int):
+def run_experiments(logdirs: list[str], n_seeds: int):
+    handles = list[AsyncResult]()
     with Pool(8) as pool:
-        handles = list()
         for seed in range(n_seeds):
-            for exp in experiments:
-                print(f"Queuing {exp.logdir} - seed {seed}")
-                h = pool.apply_async(run_experiment, (exp, seed))
+            for logdir in logdirs:
+                h = pool.apply_async(run_experiment, (logdir, seed))
                 handles.append(h)
 
         for h in handles:
             h.get()
 
 
-# TODO: investigate why it does not work when starting with DQN
 def create_experiments(n_steps: int):
-    experiments = list[marl.Experiment]()
-    for train_dqn in [False]:
+    logdirs = list[str]()
+    for train_dqn in [False, True]:
         for n_agents in range(1, 5):
-            map_name = f"maps/subgraph-{n_agents}agents.toml"
+            map_name = f"../maps/subgraph-{n_agents}agents.toml"
             env = LLE.from_file(map_name).obs_type("layered").single_objective()
             world = env.world
             env = marlenv.Builder(env).agent_id().build()
@@ -73,8 +69,7 @@ def create_experiments(n_steps: int):
             if train_dqn:
                 qnetwork = marl.nn.model_bank.CNN.from_env(env)
                 policy = marl.policy.EpsilonGreedy.linear(1.0, 0.05, 100_000)
-                test_policy = marl.policy.EpsilonGreedy.constant(1.0)
-                algo = marl.algo.DQN(qnetwork, policy, test_policy)
+                algo = marl.algo.DQN(qnetwork, policy)
                 dqn_trainer = marl.training.DQNTrainer(
                     qnetwork,
                     policy,
@@ -89,15 +84,13 @@ def create_experiments(n_steps: int):
             else:
                 algo = None
                 trainer = LocalGraphTrainer(finder, world, None)
-            logdir = "logs/dqn-" if train_dqn else "logs/"
+            logdir = "logs/dqn" if train_dqn else "logs/"
             logdir += f"{n_agents}-agents"
-            if os.path.exists(logdir):
-                shutil.rmtree(logdir)
-            exp = marl.Experiment.create(logdir=logdir, trainer=trainer, algo=algo, n_steps=n_steps, test_interval=0, env=env)
-            experiments.append(exp)
-    return experiments
+            logdirs.append(logdir)
+            marl.Experiment.create(logdir=logdir, trainer=trainer, algo=algo, n_steps=n_steps, test_interval=0, env=env)
+    return logdirs
 
 
 if __name__ == "__main__":
-    logdirs = create_experiments(1_000_000)
-    run_experiments(logdirs, 50)
+    logdirs = create_experiments(100)
+    run_experiments(logdirs, 2)
