0d00b945b4ea1ef228cdfa75e782654e7f957eb1
Yannick Molinghen <yannick.molinghen@ulb.be>
2024-11-27 17:36:48 +0100

    Refactor

--- src/marl/env/connectn/env.py
+++ src/marl/env/connectn/env.py
@@ -1,17 +1,10 @@
-"""Connect4 Monte Carlo main."""
-
-from marlenv import MARLEnv, DiscreteActionSpace
 import numpy as np
+from marlenv import MARLEnv, DiscreteActionSpace, Step, State, Observation
 
-from marlenv import Observation, State, Step
-from marl.env.connect4 import GameBoard, StepResult
-from marl.algo import MTCS
-
-# MCTS move computation time
-PROCESS_TIME = 5
+from .board import GameBoard, StepResult
 
 
-class C4Env(MARLEnv[DiscreteActionSpace]):
+class ConnectN(MARLEnv[DiscreteActionSpace]):
     def __init__(self, width: int = 7, height: int = 6, n: int = 4):
         self.board = GameBoard(width, height, n)
         action_space = DiscreteActionSpace(1, self.board.width)
@@ -54,27 +47,3 @@ class C4Env(MARLEnv[DiscreteActionSpace]):
 
     def render(self):
         self.board.show()
-
-
-def main():
-    env = C4Env(width=10, height=10, n=4)
-    board = env.board
-    mcts = MTCS(env, iteration_limit=1000, n_adversaries=1)
-    done = False
-    while not done:
-        env.render()
-        if board.turn == -1:
-            col = int(input(f"Enter the column (0 to {board.width-1}): "))
-        else:
-            print("Computer's turn, please wait...")
-            # root = Node(parent=None, board=board.board, turn=montecarlo.symbol)
-            # line, col = montecarlo.compute_move(root)
-            col = mcts.search(env.get_state())[0]
-        done = env.step([col]).is_terminal
-        print()
-    env.render()
-    print("The game is over")
-
-
-if __name__ == "__main__":
-    main()
--- src/test.py
+++ src/test.py
@@ -1,22 +1,29 @@
-from lle import LLE
-import marlenv
-from time import time
-from marl.algo import mcts
+from marl.algo import MTCS
+from marl.env import ConnectN
 
-env = LLE.level(1).single_objective()
-env = marlenv.Builder(env).time_limit(40).build()
-env.seed(0)
-obs = env.reset()
+# MCTS move computation time
+PROCESS_TIME = 5
 
-done = truncated = False
-while not done and not truncated:
-    env.render("human")
-    start = time()
-    action = mcts.search(env, time_limit_ms=1000)
-    print(action)
-    print(f"Search duration: {time() - start}")
-    _, _, done, truncated, _ = env.step(action)
-    print("trucated", truncated, "done", done)
 
-env.render("human")
-input("Press Enter to continue...")
+def main():
+    env = ConnectN(width=5, height=10, n=4)
+    board = env.board
+    mcts = MTCS(env, iteration_limit=1000, n_adversaries=1, policy="random")
+    done = False
+    while not done:
+        env.render()
+        if board.turn == -1:
+            col = int(input(f"Enter the column (0 to {board.width-1}): "))
+        else:
+            print("Computer's turn, please wait...")
+            # root = Node(parent=None, board=board.board, turn=montecarlo.symbol)
+            # line, col = montecarlo.compute_move(root)
+            col = mcts.search(env.get_state())[0]
+        done = env.step([col]).is_terminal
+        print()
+    env.render()
+    print("The game is over")
+
+
+if __name__ == "__main__":
+    main()
