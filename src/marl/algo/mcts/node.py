ecc217252ed2232152cb972f857db6d6725808b0
Yannick Molinghen <yannick.molinghen@ulb.be>
2024-11-28 11:42:29 +0100

    MCTS working on lvl 1 and 2

--- src/marl/algo/mcts/node.py
+++ src/marl/algo/mcts/node.py
@@ -4,6 +4,16 @@ import numpy as np
 
 
 class Node:
+    parent: "Node | None"
+    reward: float
+    action: tuple[int, ...]
+    """The action taken from the parent to reach the current node."""
+    num_visits: int
+    total_value: float
+    is_terminal: bool
+    current_player: int
+    children: list["Node"]
+
     def __init__(
         self,
         state: State[np.ndarray],
@@ -15,25 +25,30 @@ class Node:
     ):
         self.state = state
         self.parent = parent
+        # if parent is None:
+        #    parent_value = 0.0
+        #    sign = 1.0
+        # else:
+        #    parent_value = parent.reward
+        #    sign = -1.0 if parent.current_player == 0 else 1.0
+        # self.reward = sign * parent_value + reward
         if parent is None:
-            parent_value = 0.0
-            sign = 1.0
+            self.reward = 0
         else:
-            parent_value = parent.value
-            sign = -1.0 if parent.current_player == 0 else 1.0
-        self.value = sign * parent_value + reward
+            self.reward = reward if parent.current_player == 0 else -reward
         self.action = action_taken or tuple()
-        """The action taken from the parent to reach the current node."""
         self.num_visits = 0
         self.total_value = 0.0
         self.is_terminal = is_terminal
         self.current_player = turn
-        self.children = list[Node]()
-        """The reward that the player received when expanding the node."""
+        self.children = []
 
     @staticmethod
     def root(state: State[np.ndarray]):
-        return Node(state, None, None, False, 0, 0.0)
+        root = Node(state, None, None, False, 0, 0.0)
+        # The number of visits is initializes to 1 to be expanded on the first iteration
+        # root.num_visits = 1
+        return root
 
     @property
     def avg_value(self):
@@ -46,14 +61,26 @@ class Node:
         return len(self.children) == 0
 
     @property
+    def is_root(self):
+        return self.parent is None
+
+    @property
     def best_action(self):
         return max(self.children, key=lambda item: item.avg_value).action
 
-    def backpropate(self, value: float, gamma: float):
-        self.total_value += value
+    def backpropate(self, child_value: float, gamma: float):
         self.num_visits += 1
-        if self.parent is not None:
-            self.parent.backpropate(gamma * value, gamma)
+        if self.parent is None:
+            # It is not necessary to compute anything for the root node
+            return
+
+        # Since we work with MDPs where the reward happens on transitions,
+        # the value that has to be notified to the parent is the sum of the
+        # discounted child value with the reward that was obtained on the
+        # transition.
+        future_returns = self.reward + gamma * child_value
+        self.total_value += future_returns
+        self.parent.backpropate(future_returns, gamma)
 
     def ucb1(self, exploration_value: float) -> float:
         assert self.parent is not None, "Can not calculate UCB1 for root node"
