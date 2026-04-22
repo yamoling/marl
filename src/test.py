efb4109a7b79a4d72661c408fca6322b218c056a
Yannick Molinghen <yannick.moinghen@ulb.be>
2026-04-22 12:57:43 +0200

    Option-critic KL-div early stopping

--- src/test.py
+++ src/test.py
@@ -6,10 +6,9 @@ import lle
 
 import marl
 from marl.nn import mixers
-from marl.nn.model_bank import actor_critics
 from marl.nn.model_bank import options as options_nn
 from marl.policy import EpsilonGreedy
-from marl.training import PPO, PPOC
+from marl.training import PPOC
 
 
 def main():
@@ -19,13 +18,12 @@ def main():
     trainer = PPOC(
         oc,
         env.n_agents,
-        mixer=marl.nn.mixers.VDN.from_env(env),
+        mixer=mixers.VDN.from_env(env),
         option_train_policy=EpsilonGreedy.linear(1.0, 0.05, 50_000),
         train_interval=32,
+        early_stopping_kl=None,
     )
-    trainer = PPO(actor_critics.CNN_ActorCritic.from_env(env), mixer=mixers.VDN.from_env(env), early_stopping_kl=0.015)
-    logdir = f"logs/{env.name}-{trainer.name}"
-    logdir = "test"
+    logdir = f"logs/{env.name}-{trainer.name}-kl_{trainer.early_stopping_kl}"
     exp = marl.Experiment.create(env, 1_000_000, trainer=trainer, test_interval=5000, logdir=logdir, save_weights=True)
     exp.run(seeds=[0], n_tests=5, disabled_gpus=[1, 2, 3], n_parallel=16, fill_strategy="scatter")
 
