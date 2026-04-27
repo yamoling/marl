88bd1831ad50aad7517631b7aa228f7f6250983a
Yannick Molinghen <yannick.moinghen@ulb.be>
2026-04-27 23:50:26 +0200

    wip

--- src/test.py
+++ src/test.py
@@ -16,29 +16,29 @@ def main():
         lle.level(6)
         .obs_type("layered")
         .state_type("state")
-        .pbrs(gamma=1.0)
+        .pbrs(gamma=1.0, reward_value=1.0, lasers_to_reward=[(4, 0), (6, 12)])
         .builder()
         .agent_id()
         .time_limit(78)
-        .extra_noise(NOISE_SIZE)
+        # .extra_noise(NOISE_SIZE)
         .build()
     )
 
-    # trainer = MAVEN(
-    #     qnetworks.QCNN.from_env(env),
-    #     marl.policy.EpsilonGreedy.linear(1.0, 0.05, 50_000),
-    #     marl.models.EpisodeMemory(5_000),
-    #     NOISE_SIZE,
-    #     env.n_actions,
-    #     env.n_agents,
-    #     env.state_size,
-    #     env.state_extras_size,
-    #     mixer=mixers.VDN.from_env(env),
-    #     test_policy=marl.policy.ArgMax(),
-    #     grad_norm_clipping=10.0,
-    #     batch_size=16,
-    #     train_interval=(1, "episode"),
-    # )
+    trainer = MAVEN(
+        qnetworks.QCNN.from_env(env),
+        marl.policy.EpsilonGreedy.linear(1.0, 0.05, 50_000),
+        marl.models.EpisodeMemory(5_000),
+        NOISE_SIZE,
+        env.n_actions,
+        env.n_agents,
+        env.state_size,
+        env.state_extras_size,
+        mixer=mixers.VDN.from_env(env),
+        test_policy=marl.policy.ArgMax(),
+        grad_norm_clipping=10.0,
+        batch_size=16,
+        train_interval=(1, "episode"),
+    )
     trainer = DQN(
         qnetworks.QCNN.from_env(env),
         marl.policy.EpsilonGreedy.linear(1.0, 0.05, 50_000),
@@ -49,8 +49,8 @@ def main():
         batch_size=16,
         train_interval=(1, "episode"),
     )
-    logdir = f"logs/{trainer.name}-{env.name}"
-    logdir = "test"
+    logdir = f"logs/{trainer.name}-{env.name}-no-noise"
+    # logdir = "test"
     exp = marl.Experiment.create(
         env,
         1_000_000,
@@ -60,7 +60,7 @@ def main():
         save_weights=False,
         replace_if_exists=True,
     )
-    exp.run(seeds=30, n_tests=1, disabled_gpus=[1, 2, 3], fill_strategy="scatter", quiet=False)
+    exp.run(seeds=30, n_tests=1, fill_strategy="scatter", quiet=False, n_parallel=2)
 
 
 if __name__ == "__main__":
